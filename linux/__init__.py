690c4154b5dac9204fac4180b4e9c16e92defae2
Choseongyul <tjdduf0504@naver.com>
2026-09-14 23:27:42 +0900

    chore: retain only ps and detector plugins

--- linux/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Add Docker v2 alongside the official Volatility Linux plugins."""
-
-from pkgutil import extend_path
-
-# Keep official linux.pslist, linux.mountinfo, etc. importable with -p plugins.
-__path__ = extend_path(__path__, __name__)
